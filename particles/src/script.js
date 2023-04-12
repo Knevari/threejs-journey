bda4a2ca764b615c3ae6120cf784e006666450a0
Knevari <winresource000@gmail.com>
2023-04-12 19:05:12 -0300

    add particles

---
+++ particles/src/script.js
@@ -0,0 +1,156 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+const particleTexture = textureLoader.load("/textures/particles/2.png");
+
+/**
+ * Particles
+ */
+const particlesCount = 20000;
+const particlesGeometry = new THREE.BufferGeometry();
+const positions = new Float32Array(particlesCount * 3);
+// const colors = new Float32Array(particlesCount * 3);
+
+for (let i = 0; i < particlesCount * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10;
+  // colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+);
+// particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+  color: "#ffffff",
+  map: particleTexture,
+  // alphaMap: particleTexture,
+  // alphaTest: 0.01,
+  // depthTest: false,
+  // depthWrite: false,
+  transparent: true,
+  // blending: THREE.AdditiveBlending,
+  // vertexColors: true,
+});
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+camera.far = 32;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // particles.rotation.y = elapsedTime * 0.2;
+
+  for (let i = 0; i < particlesCount; i++) {
+    const i3 = i * 3;
+    const xi = i3;
+    const yi = i3 + 1;
+    const zi = i3 + 2;
+
+    particlesGeometry.attributes.position.array[yi] = Math.min(
+      Math.sin(
+        elapsedTime + particlesGeometry.attributes.position.array[xi] * 2
+      ),
+      Math.PI * 0.02
+    );
+
+    particlesGeometry.attributes.position.array[yi] += Math.min(
+      Math.sin(
+        elapsedTime + particlesGeometry.attributes.position.array[zi] * 2
+      ),
+      Math.PI * 0.02
+    );
+  }
+
+  particlesGeometry.attributes.position.needsUpdate = true;
+
+  // camera.position.set(Math.sin(elapsedTime) * 2, 0, Math.cos(elapsedTime) * 2);
+  // camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
